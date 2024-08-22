f028db2f5f43acb8d2c468b2bd42c96d5a67466a
PoojithaAdobe19 <poojitha.adobe19@gmail.com>
2024-08-21 21:14:14 -0400

    Update launch-setup.js

---
+++ Adobe-Launch/Sample-Implementation/launch-setup.js
@@ -0,0 +1,5 @@
+// Adobe Launch sample implementation
+document.getElementById('purchaseButton').addEventListener('click', function() {
+    // Track the purchase button click event
+    _satellite.track('purchaseButtonClick');
+});
